c3ca8cbf766ab442d13684a29093f5bfedc2da60
zzb610 <bwyuchi@163.com>
2020-12-18 05:13:50 +0000

    add setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="factest",
     version="0.0.1",
-    keywords=("pip", "factest", "alpha factor", "quant"),
+    keywords=["pip", "factest", "alpha factor", "quant"],
     description="",
     long_description="A Fantistic Factor Analysising Tools For Quant",
     license="MIT Licence",
